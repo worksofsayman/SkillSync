0f1fd1058a4ee9bd1d35d8338a6617ebfd8d6433
Sayman7 <buisnesssayman@gmail.com>
2025-01-22 20:58:21 -0500

    react setup

---
+++ skillsync/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.css';
+function App() {
+  return (
+   <>
+   <h1 className= 'text-right'>Hello World!</h1></>
+  );
+}
+
+export default App;
